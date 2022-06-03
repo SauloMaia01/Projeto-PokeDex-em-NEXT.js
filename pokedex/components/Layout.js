edfc382e508b5680440d84a3dd54b54b3fd18415
SauloMaia01 <saulormaia@hotmail.com>
2022-06-03 20:52:10 -0300

    tentativa de começar o projeto

--- pokedex/components/Layout.js
+++ pokedex/components/Layout.js
@@ -3,19 +3,17 @@ import Footer from './Footer'
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function Layout(children) {
+export default function Layout({children}) {
     return(
         <>
             <Head>
-                <Link 
-                    rel="shortcut" 
-                    href="/images/favicon.ico"
-                />
+            
+                
                 <title>PokeNext</title>
             </Head>
             <Navbar />
             <main>{children}</main>
-            <footer />
+            <Footer />
         </>
     )
 }
